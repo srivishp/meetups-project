f3c33ca16d48ea6ec7c9d2656c8e3a4e84e38857
Sri Vishnu <srivishp@gmail.com>
2023-10-27 16:39:23 +0530

    Wrap Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "@/components/layout/Layout";
+import "../styles/globals.css";
+
+// Component contains whatever is displayed on screen, so we can wrap it with other components like Layout
+// if we want to display them on every screen
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
