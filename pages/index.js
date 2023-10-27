2302f751f72b37dcd0ff288939bcb8a7732c6fb1
Sri Vishnu <srivishp@gmail.com>
2023-10-27 16:19:02 +0530

    Add New Meetup Page

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,27 @@
 import MeetupList from "@/components/meetups/MeetupList";
 
-const DUMMY_MEETUPS =[
-
-{
-id: 'm1',
-title: 'A First Meetup',
-image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/800px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg",
-address: "Some address, some city etc etc...",
-description: "This is the first meetup!"
-
-},
-
-{
-  id: 'm2',
-  title: 'A Second Meetup',
-  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Lady_Liberty_under_a_blue_sky_%28cropped%29.jpg/800px-Lady_Liberty_under_a_blue_sky_%28cropped%29.jpg",
-  address: "Some address, some city etc etc...",
-  description: "This is the second meetup!"
-
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/800px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg",
+    address: "Some address, some city etc etc...",
+    description: "This is the first meetup!",
   },
 
-]
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Lady_Liberty_under_a_blue_sky_%28cropped%29.jpg/800px-Lady_Liberty_under_a_blue_sky_%28cropped%29.jpg",
+    address: "Some address, some city etc etc...",
+    description: "This is the second meetup!",
+  },
+];
 
-function Homepage() {
-  return <MeetupList meetups={}/>;
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
