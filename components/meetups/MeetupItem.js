3106a1e36cf2703525c64602c0d6c63e17226188
Sri Vishnu <srivishp@gmail.com>
2023-10-30 11:50:22 +0530

    Programmatic navigation using router.push()

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,17 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import { useRouter } from "next/router";
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
+  // React hooks must always be used at the root level of a component
+  const router = useRouter();
+
+  function showDetailsHandler() {
+    // todo: Programmatic Navigation
+    // * push() method pushes a new page onto a stack of pages
+    // Getting the id via props from meetup list
+    router.push("/" + props.id);
+  }
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +23,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
