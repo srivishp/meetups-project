0c54d2c9be483f0ad73a12f5235162a7740b925a
Sri Vishnu <srivishp@gmail.com>
2023-10-30 13:40:01 +0530

    Add meetup detail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/800px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg"
+      title="A First Meetup"
+      address="Eiffel Tower, Paris"
+      description="Meetup Description"
+    />
+  );
+}
+
+export default MeetupDetails;
